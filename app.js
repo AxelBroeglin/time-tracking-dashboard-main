df6ec4f49539d991014e12f04b2ed702be666a80
Axel Broeglin <axelbroeglin@gmail.com>
2022-04-20 13:21:49 +0100

    Can access and use data from Json

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+// fetch("./data.json")
+// .then(response => {
+//    return response.json();
+// })
+// .then(jsondata => console.log(jsondata))
+// .then(function(){
+//     //const text = jsondata;
+//     //const obj = JSON.parse(text);
+//     const para = document.createElement("p");
+//     //para.innerText = obj.name;
+//     document.body.appendChild(para);
+// })
+// // .then(function showHeroes(jsonObj){
+// //    showHeroes(jsonObj);
+// //  });
+
+
+// //console logs the info, but can't access the info.
+
 fetch("./data.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata))
-.then(function(){
-    const para = document.createElement("p");
-para.innerText = "This is a paragraph";
-document.body.appendChild(para);
-})
-.then(function showHeroes(jsonObj){
-    showHeroes(jsonObj)
-    console.log(title);
-})
-
-
-//console logs the info, but can't access the info.
-
-
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        console.log(data[1].title);
+        const para = document.createElement("p");
+        para.innerText = data[1].title;
+        document.body.appendChild(para);
+    })
