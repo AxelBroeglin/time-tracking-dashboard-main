591b8191d15283b52c9d74edc84f9ccf90523079
AxelBroeglin <axelbroeglin@gmail.com>
2022-04-20 21:27:23 +0100

    HTML elements created w/ loop

--- app.js
+++ app.js
@@ -3,30 +3,28 @@ fetch("./data.json")
     .then(function(resp){
         return resp.json();
     })
+    //Creates the Data function that will be called all along
     .then(function(data){
-        const para = document.createElement("p");
-        para.innerText = data[1].title;
-        document.body.appendChild(para);
-        const test =  data[1].timeframes.weekly.previous;
-        console.log(test);
-        console.log(data.length);
-
+        //Creates the loop that will create all the elements, using the number of object in data.
         for(let i = 0; i < data.length; i++){ 
+            //Variable to create a section each time a loop is initiated
             const card = document.createElement("section");
-            document.body.appendChild(card);
+            //'Self care' is 2 words, can't pass as a class, so we split the space, and call the first entry of the array
+            let title = data[i].title;
+            const cardClass = title.split(" ")[0];
+            //Adds classes to the cards using their titles
+            card.classList.add('activity-'+cardClass);
+            //Places the loop at the end of Main
+            document.getElementById('main-container').appendChild(card);
+            //Creates the HTML elements + the looped objects from JSON.
+            //Will need to be updated with the Daily, Weekly or Monthly registered from the click on the button on the page (so data[i].timeframes.xxx + Last xxx)
             card.insertAdjacentHTML(`afterbegin`, 
-            '<div class="" id=""> <div><h2 class="">' + data[i].title + 
-            '</h2><span class="">...</span></div><p class="">' + data[i].timeframes.daily.current +
-            'hrs<span class="">Last day - ' + data[i].timeframes.daily.previous + '</span></p></div>')
+            '<div class="" id=""><div><h2 class="">' 
+            + data[i].title + 
+            '</h2><span class="">...</span></div><p class="">' 
+            + data[i].timeframes.daily.current +
+            'hrs<span class="">Last day - ' 
+            + data[i].timeframes.daily.previous + 
+            '</span></p></div>')
         }
     })
-
-    // div
-    //     div
-    //         div 
-    //             title ... 
-    //         /div
-    //         32hrs
-    //         last week
-    //     /div
-    // /div
