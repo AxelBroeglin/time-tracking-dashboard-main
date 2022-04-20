a9bdd663b6a14277208f3e697e101c1f171b6764
AxelBroeglin <axelbroeglin@gmail.com>
2022-04-20 20:06:30 +0100

    For loop for every object in JSON file

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-// fetch("./data.json")
-// .then(response => {
-//    return response.json();
-// })
-// .then(jsondata => console.log(jsondata))
-// .then(function(){
-//     //const text = jsondata;
-//     //const obj = JSON.parse(text);
-//     const para = document.createElement("p");
-//     //para.innerText = obj.name;
-//     document.body.appendChild(para);
-// })
-// // .then(function showHeroes(jsonObj){
-// //    showHeroes(jsonObj);
-// //  });
-
-
-// //console logs the info, but can't access the info.
-
+//Fetch the local JSON file
 fetch("./data.json")
     .then(function(resp){
         return resp.json();
     })
     .then(function(data){
-        console.log(data[1].title);
         const para = document.createElement("p");
         para.innerText = data[1].title;
         document.body.appendChild(para);
+        const test =  data[1].timeframes.weekly.previous;
+        console.log(test);
+        console.log(data.length);
+
+        for(let i = 0; i < data.length; i++){ 
+            const card = document.createElement("section");
+            document.body.appendChild(card);
+            card.insertAdjacentHTML(`afterbegin`, 
+            '<div class="" id=""> <div><h2 class="">' + data[i].title + 
+            '</h2><span class="">...</span></div><p class="">' + data[i].timeframes.daily.current +
+            'hrs<span class="">Last day - ' + data[i].timeframes.daily.previous + '</span></p></div>')
+        }
     })
 
+    // div
+    //     div
+    //         div 
+    //             title ... 
+    //         /div
+    //         32hrs
+    //         last week
+    //     /div
+    // /div
